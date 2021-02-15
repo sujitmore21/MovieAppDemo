0ccc61c5d3a32eb1bf23cd30213296b8a7ed219a
Sujit More <sujitmore21@gmail.com>
2021-02-15 11:35:44 +0530

    movie app using MVVM

---
+++ app/src/main/java/com/sujitmo/movieappmvvm/viewmodel/LoginViewModel.java
@@ -0,0 +1,61 @@
+package com.sujitmo.movieappmvvm.viewmodel;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.sujitmo.movieappmvvm.BR;
+import com.sujitmo.movieappmvvm.model.User;
+
+public class LoginViewModel extends BaseObservable {
+    private User user;
+
+    private String successMessage = "Login was successful";
+    private String errorMessage = "Email or Password not valid";
+
+    @Bindable
+    private String toastMessage = null;
+
+    private void setToastMessage(String toastMessage){
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public void setUserEmail(String email) {
+        user.setEmail(email);
+        notifyPropertyChanged(BR.userEmail);
+    }
+
+    @Bindable
+    public String getUserEmail() {
+        return user.getEmail();
+    }
+
+    @Bindable
+    public String getUserPassword() {
+        return user.getPassword();
+    }
+
+    public void setUserPassword(String password) {
+        user.setPassword(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    public LoginViewModel() {
+        user = new User("","");
+    }
+
+    public void onLoginClicked() {
+        if (isInputDataValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+
+    public boolean isInputDataValid() {
+        return !TextUtils.isEmpty(getUserEmail()) && Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches() && getUserPassword().length() > 5;
+    }
+
+}
